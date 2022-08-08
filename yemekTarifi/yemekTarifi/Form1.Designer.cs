4c9fd024e6ee23949162a8030bf350fa2a34b9a9
Oğuzhan Bilal Kahraman <73184456+OguzhanbilalKahraman@users.noreply.github.com>
2022-08-08 15:46:41 +0300

    Add files via upload

---
+++ yemekTarifi/yemekTarifi/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace yemekTarifi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listFbox = new System.Windows.Forms.ListBox();
+            this.btnAsamaEkle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listFbox
+            // 
+            this.listFbox.FormattingEnabled = true;
+            this.listFbox.Location = new System.Drawing.Point(51, 49);
+            this.listFbox.Name = "listFbox";
+            this.listFbox.Size = new System.Drawing.Size(500, 329);
+            this.listFbox.TabIndex = 0;
+            // 
+            // btnAsamaEkle
+            // 
+            this.btnAsamaEkle.Location = new System.Drawing.Point(592, 197);
+            this.btnAsamaEkle.Name = "btnAsamaEkle";
+            this.btnAsamaEkle.Size = new System.Drawing.Size(136, 36);
+            this.btnAsamaEkle.TabIndex = 1;
+            this.btnAsamaEkle.Text = "Sıradaki Aşamayı Gör";
+            this.btnAsamaEkle.UseVisualStyleBackColor = true;
+            this.btnAsamaEkle.Click += new System.EventHandler(this.btnAsamaEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAsamaEkle);
+            this.Controls.Add(this.listFbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listFbox;
+        private System.Windows.Forms.Button btnAsamaEkle;
+    }
+}
+
---
+++ yemekTarifi/yemekTarifi/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yemekTarifi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string[] stages =  {"Makarna Tarifi","1-Tencereye 2 litre su ekleyin ve kaynatın","2-Su kaynadıktan sonra 1 paket makarnayı tencereye ilave edin ve 8-10 dakika karıştırın","3-Süre geçtikten sonra süzün,bir bardak suyu saklayın ve tencerenizde soğan salça kavurun.","4-makarnayı ilave edin karıştırın ve ayırdığınız suyu da dökün.","5-Kendi referansına göre baharatlarını ayarlayın.","Afiyet olsun." };
+        int index = 0;
+        private void btnAsamaEkle_Click(object sender, EventArgs e)
+        {
+            //int index=0;
+            if (index <stages.Length)
+            {
+                listFbox.Items.Add(stages[index]);
+                index++;
+            }
+            else
+            {
+                MessageBox.Show("Tarif tamamlanmistir Afiyet olsun.");
+            }
+        }
+    }
+}
